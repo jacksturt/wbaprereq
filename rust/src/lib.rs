550ed02fb40c90e3008b8e6cd7521f32c47820d2
jacksturt <jack@sturt.io>
2024-02-02 18:14:02 -0700

    add enroll

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -11,9 +11,10 @@ use solana_client::rpc_client::RpcClient;
 use solana_program::{
     pubkey::Pubkey,
     system_instruction::transfer,
+    system_program
     };
-    use std::str::FromStr;
-    use crate::programs::wba_prereq::{WbaPrereqProgram, CompleteArgs, UpdateArgs};
+use std::str::FromStr;
+use crate::programs::wba_prereq::{WbaPrereqProgram, CompleteArgs, UpdateArgs};
 #[test]
 fn keygen(
 ) {
@@ -118,6 +119,27 @@ println!(
 fn enroll(){
     const RPC_URL: &str = "https://api.devnet.solana.com";
     let rpc_client = RpcClient::new(RPC_URL);
-
+    let signer = read_keypair_file("wba-wallet.json").expect("Couldn't find wallet file");
+    let prereq = WbaPrereqProgram::derive_program_address(&[b"prereq",
+signer.pubkey().to_bytes().as_ref()]);
+let args = CompleteArgs {
+    github: b"jacksturtevant".to_vec()
+    };
+    let blockhash = rpc_client
+.get_latest_blockhash()
+.expect("Failed to get recent blockhash");
+let transaction = WbaPrereqProgram::complete(
+    &[&signer.pubkey(), &prereq, &system_program::id()],
+    &args,
+    Some(&signer.pubkey()),
+    &[&signer],
+    blockhash
+    );
+    let signature = rpc_client
+.send_and_confirm_transaction(&transaction)
+.expect("Failed to send transaction");
+// Print our transaction out
+println!("Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet",
+signature);
 }
 }
--- rust/src/programs/wba_prereq.rs
+++ rust/src/programs/wba_prereq.rs
@@ -1,87 +1,87 @@
 use solana_idlgen::idlgen;
 idlgen!({
-    version: "0.1.0";
-    name: "wba_prereq";
-    instructions: [
+    "version": "0.1.0",
+    "name": "wba_prereq",
+    "instructions": [
       {
-        name: "complete";
-        accounts: [
+        "name": "complete",
+        "accounts": [
           {
-            name: "signer";
-            isMut: true;
-            isSigner: true;
+            "name": "signer",
+            "isMut": true,
+            "isSigner": true
           },
           {
-            name: "prereq";
-            isMut: true;
-            isSigner: false;
+            "name": "prereq",
+            "isMut": true,
+            "isSigner": false
           },
           {
-            name: "systemProgram";
-            isMut: false;
-            isSigner: false;
+            "name": "systemProgram",
+            "isMut": false,
+            "isSigner": false
           }
-        ];
-        args: [
+        ],
+        "args": [
           {
-            name: "github";
-            type: "bytes";
+            "name": "github",
+            "type": "bytes"
           }
-        ];
+        ]
       },
       {
-        name: "update";
-        accounts: [
+        "name": "update",
+        "accounts": [
           {
-            name: "signer";
-            isMut: true;
-            isSigner: true;
+            "name": "signer",
+            "isMut": true,
+            "isSigner": true
           },
           {
-            name: "prereq";
-            isMut: true;
-            isSigner: false;
+            "name": "prereq",
+            "isMut": true,
+            "isSigner": false
           },
           {
-            name: "systemProgram";
-            isMut: false;
-            isSigner: false;
+            "name": "systemProgram",
+            "isMut": false,
+            "isSigner": false
           }
-        ];
-        args: [
+        ],
+        "args": [
           {
-            name: "github";
-            type: "bytes";
+            "name": "github",
+            "type": "bytes"
           }
-        ];
+        ]
       }
-    ];
-    accounts: [
+    ],
+    "accounts": [
       {
-        name: "PrereqAccount";
-        type: {
-          kind: "struct";
-          fields: [
+        "name": "PrereqAccount",
+        "type": {
+          "kind": "struct",
+          "fields": [
             {
-              name: "github";
-              type: "bytes";
+              "name": "github",
+              "type": "bytes"
             },
             {
-              name: "key";
-              type: "publicKey";
+              "name": "key",
+              "type": "publicKey"
             }
-          ];
-        };
+          ]
+        }
       }
-    ];
-    errors: [
+    ],
+    "errors": [
       {
-        code: 6000;
-        name: "InvalidGithubAccount";
-        msg: "Invalid Github account";
+        "code": 6000,
+        "name": "InvalidGithubAccount",
+        "msg": "Invalid Github account"
       }
-    ];
+    ],
     "metadata": {
         "address": "HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1"
-        };
-  })
+        }
+  });
